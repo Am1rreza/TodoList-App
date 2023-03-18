f4d141f48ba7b8e522f44d6500f5524619af993c
Amirreza <amirreza.m13825@gmail.com>
2023-03-18 15:41:07 +0330

    create /api/todos api and map on todos

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
+import axios from "axios";
 import { HiCheck, HiOutlineTrash, HiOutlinePencil } from "react-icons/hi2";
+import useSWR from "swr";
+
+const fetcher = async () => {
+  const { data } = await axios.get("/api/todos");
+  return data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useSWR("getTodos", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className="bg-gray-50 min-h-screen">
       <nav className=" w-full bg-white shadow-sm flex justify-center py-4 mb-6">
@@ -9,48 +21,27 @@ export default function Home() {
       <div className="container p-4 xl:max-w-screen-xl mx-auto">
         <section className="flex items-center justify-center">
           <div className="w-full max-w-screen-md bg-white p-2 md:p-4 rounded-xl">
-            <div className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl">
-              <span>Task 1</span>
-              <div className="flex gap-x-3 items-center">
-                <button className="">
-                  <HiCheck className="w-6 h-6 stroke-green-400" />
-                </button>
-                <button className="">
-                  <HiOutlineTrash className="w-6 h-6 stroke-red-400" />
-                </button>
-                <button>
-                  <HiOutlinePencil className="w-6 h-6 stroke-blue-400" />
-                </button>
-              </div>
-            </div>
-            <div className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl">
-              <span>Task 1</span>
-              <div className="flex gap-x-3 items-center">
-                <button className="">
-                  <HiCheck className="w-6 h-6 stroke-green-400" />
-                </button>
-                <button className="">
-                  <HiOutlineTrash className="w-6 h-6 stroke-red-400" />
-                </button>
-                <button>
-                  <HiOutlinePencil className="w-6 h-6 stroke-blue-400" />
-                </button>
-              </div>
-            </div>
-            <div className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl">
-              <span>Task 1</span>
-              <div className="flex gap-x-3 items-center">
-                <button className="">
-                  <HiCheck className="w-6 h-6 stroke-green-400" />
-                </button>
-                <button className="">
-                  <HiOutlineTrash className="w-6 h-6 stroke-red-400" />
-                </button>
-                <button>
-                  <HiOutlinePencil className="w-6 h-6 stroke-blue-400" />
-                </button>
-              </div>
-            </div>
+            {data.todos.map((todo) => {
+              return (
+                <div
+                  key={todo.id}
+                  className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl"
+                >
+                  <span>{todo.title}</span>
+                  <div className="flex gap-x-3 items-center">
+                    <button className="">
+                      <HiCheck className="w-6 h-6 stroke-green-400" />
+                    </button>
+                    <button className="">
+                      <HiOutlineTrash className="w-6 h-6 stroke-red-400" />
+                    </button>
+                    <button>
+                      <HiOutlinePencil className="w-6 h-6 stroke-blue-400" />
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </section>
       </div>
