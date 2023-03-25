28b9c89d374fa4eb8db9c7fd63ef8b968fc62620
Amirreza <amirreza.m13825@gmail.com>
2023-03-25 16:42:10 +0430

    securing page on the server-side

--- pages/protected-ssr.js
+++ pages/protected-ssr.js
@@ -1,13 +1,25 @@
 import Layout from "@/containers/Layout/Layout";
 import { getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 const ProtectedSSR = () => {
   const { data: session, status } = useSession();
 
   return (
     <Layout>
-      <h1 className="text-2xl">Protected page from server</h1>
+      <h1 className="text-xl flex flex-col items-center text-center sm:flex-row sm:items-center gap-4">
+        <img
+          src={session.user.image}
+          alt="profile-img"
+          className="w-24 h-24 sm:w-20 sm:h-20 rounded-full"
+        />
+        <p>
+          {" "}
+          {session.user.name}, Wellcome to your{" "}
+          <span className="font-semibold">Protected SSR</span> Page
+        </p>
+      </h1>
     </Layout>
   );
 };
@@ -15,7 +27,17 @@ const ProtectedSSR = () => {
 export default ProtectedSSR;
 
 export async function getServerSideProps(context) {
-  const session = await getServerSession(context.req, context.res);
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination:
+          "/api/auth/signin?callbackUrl=http://localhost:3000/protected-ssr",
+        permanent: false,
+      },
+    };
+  }
 
   return {
     props: {
